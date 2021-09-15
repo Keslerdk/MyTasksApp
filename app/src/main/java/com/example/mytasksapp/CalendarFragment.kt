efe5333701eb1486b26b7f34ef628ce24c309e27
Daria <kd7614@yandex.ru>
2021-09-16 02:11:58 +0300

    cab

--- app/src/main/java/com/example/mytasksapp/CalendarFragment.kt
+++ app/src/main/java/com/example/mytasksapp/CalendarFragment.kt
@@ -1,12 +1,13 @@
 package com.example.mytasksapp
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.view.ActionMode.Callback
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.example.mytasksapp.data.Task
 import com.example.mytasksapp.databinding.FragmentCalendarBinding
 import com.example.mytasksapp.model.CalendarDayAdapter
 import com.example.mytasksapp.model.CalendarViewModel
@@ -18,12 +19,16 @@ class CalendarFragment : Fragment() {
     private var _binding: FragmentCalendarBinding? = null
     private val binding get() = _binding!!
 
+    private val listToDelete = mutableListOf<Task>()
+
     private val viewModel: CalendarViewModel by activityViewModels {
         CalendarViewModelFactory(
             (activity?.application as MyTaskApplication).database
                 .taskDao()
         )
     }
+
+    var myActMode: ActionMode? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,7 +38,7 @@ class CalendarFragment : Fragment() {
         _binding = FragmentCalendarBinding.inflate(inflater, container, false)
 
         binding.viewModel = viewModel
-        binding.lifecycleOwner= this
+        binding.lifecycleOwner = this
         binding.calendarFragment = this
         return binding.root
     }
@@ -54,6 +59,15 @@ class CalendarFragment : Fragment() {
         viewModel.selectedDate.observe(viewLifecycleOwner) {
             viewModel.setRelevantTasks()
         }
+
+        binding.date.isLongClickable = true
+        binding.date.setOnLongClickListener{
+            if (myActMode != null) {
+                return@setOnLongClickListener false;
+            }
+            myActMode = activity?.startActionMode(ActionModeCallback());
+            return@setOnLongClickListener true
+        }
     }
 
     /**
@@ -67,4 +81,45 @@ class CalendarFragment : Fragment() {
         private const val TAG = "CalendarFragment"
     }
 
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        when (item.itemId) {
+//            R.id.action_delete -> Toast.makeText(this.context, "Refreshing...", Toast.LENGTH_SHORT)
+//                .show()
+//            else -> return true
+//        }
+//        return false
+//    }
+
+
+//    val myActionMode = ActionMode.Callback2 {
+//
+//    }
+
+    inner class ActionModeCallback() : ActionMode.Callback {
+        override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+            mode?.menuInflater?.inflate(R.menu.menu, menu)
+            mode?.title = "Select Options"
+            return true
+        }
+
+        override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+            return false
+        }
+
+        override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
+            when (item?.itemId) {
+                R.id.action_delete -> Toast.makeText(context, "Refreshing...", Toast.LENGTH_SHORT)
+                    .show()
+                else -> return true
+            }
+            return false
+        }
+
+        override fun onDestroyActionMode(mode: ActionMode?) {
+            TODO("Not yet implemented")
+        }
+
+    }
+
 }
+
